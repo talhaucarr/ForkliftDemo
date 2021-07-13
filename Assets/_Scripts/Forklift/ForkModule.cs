68406e9ce73ab69916957400d5823da4164fa760
talhaucarr <talhaucarr@hotmail.com>
2021-07-13 15:30:33 +0300

    Added lift movement

---
+++ Assets/_Scripts/Forklift/ForkModule.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForkModule : MonoBehaviour,IForkModule
+{
+    [Header("Requires")]
+    [SerializeField] private Transform leftFork;
+    [SerializeField] private Transform rightFork;
+
+    [Header("Options")]
+    [SerializeField] private float maxHeight;
+    [SerializeField] private float minHeight;
+    [SerializeField] private float liftSpeed;
+
+    public void Lift(float vertical)
+    {
+        leftFork.position += new Vector3(0f, vertical * liftSpeed * Time.deltaTime, 0f);
+        rightFork.position += new Vector3(0f, vertical * liftSpeed * Time.deltaTime, 0f);
+
+        if (leftFork.position.y > maxHeight)
+        {
+            leftFork.position = new Vector3(leftFork.position.x, maxHeight, leftFork.position.z);
+            rightFork.position = new Vector3(rightFork.position.x, maxHeight, rightFork.position.z);
+        }
+        if (leftFork.position.y < minHeight)
+        {
+            leftFork.position = new Vector3(leftFork.position.x, minHeight, leftFork.position.z);
+            rightFork.position = new Vector3(rightFork.position.x, minHeight, rightFork.position.z);
+        }
+    }
+
+    public void PickUp()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void PutDown()
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- Assets/_Scripts/Forklift/InputModule.cs
+++ Assets/_Scripts/Forklift/InputModule.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 
 public class InputModule : MonoBehaviour
 {
+    [SerializeField] private float verticalLift;
+
     private IMovementModule _movementModule;
+    private IForkModule _forkModule;
 
     private float _horizontal;
     private float _vertical;
@@ -14,22 +17,38 @@ public class InputModule : MonoBehaviour
     void Start()
     {
         _movementModule = GetComponent<MovementModule>();
+        _forkModule = GetComponent<ForkModule>();
     }
 
     
     void FixedUpdate()
     {
-        GetInput();
-        _movementModule.HandleMotor(_vertical, _isBreaking);
-        _movementModule.HandleSteering(_horizontal);
-        _movementModule.UpdateWheels();
+        GetMovementInput();
+        GetLiftInput();
+        MovementModuleHandler();
     }
-
-    private void GetInput()
+    private void GetMovementInput()
     {
         _horizontal = Input.GetAxis("Horizontal");
         _vertical = Input.GetAxis("Vertical");
         _isBreaking = Input.GetKey(KeyCode.Space);
         Debug.Log(_isBreaking);
+    }
+    private void MovementModuleHandler()
+    {
+        _movementModule.HandleMotor(_vertical, _isBreaking);
+        _movementModule.HandleSteering(_horizontal);
+        _movementModule.UpdateWheels();
+    }
+
+    private void GetLiftInput()
+    {
+        if (Input.GetKey(KeyCode.J)) { ForkModuleHandler(verticalLift); }
+        if (Input.GetKey(KeyCode.K)) { ForkModuleHandler(-verticalLift); }
+    }
+
+    private void ForkModuleHandler(float verticalDir)
+    {
+        _forkModule.Lift(verticalDir);
     }
 }
