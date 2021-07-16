07228a0fed9fc007641014eb6487a1d3b79277ec
talhaucarr <talhaucarr@hotmail.com>
2021-07-16 11:39:38 +0300

    Added pendulum

--- Assets/_Scripts/Box/MovingPlatform.cs
+++ Assets/_Scripts/Box/MovingPlatform.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
     public GameObject palyer;
+    public bool test = true;
+
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log(other);
         if (other.gameObject.GetComponent<Tag>().Tags.Contains(Tags.Player))
         {
-            Debug.Log("here");
-            palyer.transform.parent = transform;
+            //int childct;
+            ////palyer.transform.SetParent(gameObject.transform, true);
+
+            //childct = palyer.transform.childCount;
+            //for (int i = 0; i < childct; i++)
+            //{
+            //    palyer.transform.GetChild(i).parent = palyer.transform;
+            //}
+            //palyer.transform.parent = gameObject.transform;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.GetComponent<Tag>().Tags.Contains(Tags.Player))
-        {
-            Debug.Log("burda");
-            palyer.transform.parent = null;
-        }
+        //if (other.gameObject.GetComponent<Tag>().Tags.Contains(Tags.Player))
+        //{
+        //    Debug.Log("burda");
+        //    palyer.transform.parent = null;
+        //}
     }
 }
